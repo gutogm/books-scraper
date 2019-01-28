8623da56ec1479316007bcdaa5d41efb9494a6e2
Luiz Augusto <luiz.moraes@drtis.com.br>
2019-01-28 00:25:09 -0200

    fazendo algumas refatorações

--- index.js
+++ index.js
@@ -1,67 +1,15 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const Books = require('books-lib');
-
-const page = require('./page');
+const { Books } = require('./page');
 
 const URL = 'https://kotlinlang.org/docs/books.html';
 
-let count = -1;
-const structure = {};
-
-function next(children, idx=-1) {
-  const _idx = idx + 1;
-  const element = children[`${_idx}`];
-  if (!element)
-    return null;
-  return {
-    element,
-    next: () => next(children, _idx)
-  }; 
-}
-
-function createIterator(article) {
-  return { next: () => next(article.children()) };
-}
-
-function extract(item) {
-  const { element } = item;
-  if(element) {
-    if(element.type === 'tag') {
-      if(element.name === 'h2') {
-        count += 1;
-        structure[count] = { description: '' };
-        structure.length = count + 1;
-        structure[count].title = page.getTitle(element);
-      }
-      if(element.name === 'div'){
-        structure[count].language = page.getLanguage(element);
-      };
-      if(element.name === 'a') {
-        structure[count].isbn = page.getISBN(element);
-      };
-      if(element.name === 'p'){
-        structure[count].description = `${structure[count].description} ${page.getDescription(element)}`
-      }
-    }
-  }
-}
-
 async function run(url) {
-  const { data: content } = await axios.get(url);
-  const $ = cheerio.load(content);
-  const article = $('body > div.global-layout > div > article');
-  const iterator = createIterator(article);
-  let item = iterator.next();
-  while(item) {
-    extract(item);
-    item = item.next();
-  }
-  for (let index = 0; index < structure.length; index++) {
-    const element = structure[`${index}`];
-    const book = await Books.create(element);
-    console.log(`book with id ${book.id} created.`);
-  }
+  const books = new Books();
+  // extract data from page
+  await books.extract(url);
+  // console.log(books.article);
+  books.transform();
+  console.log(books.structure);
+  // books.load();
 }
 run(URL)
   .catch(err => console.error(err));
--- page.js
+++ page.js
@@ -1,24 +1,105 @@
-module.exports = {
-  getTitle: (e) => e.children[0].data,
+const axios = require('axios');
+const cheerio = require('cheerio');
+const API = require('books-lib');
 
-  getLanguage: (e) => e.children[0].data,
+class Book {
+  static _title(e) {
+    return e.children[0].data;
+  }
+  static _language(e) {
+    return e.children[0].data;
+  }
 
-  getISBN: (e) => {
+  static _isbn(e) {
     const { href } = e.attribs;
     let isbn = 'Unavailable';
     if(href.indexOf('amazon.com') > -1) {
       isbn = href.split('/dp/')[1] || 'Unavailable';
     }
     return isbn;
-  },
+  };
 
-  getDescription: (e) => e.children
-    .map(el => {
-      if(el.type === 'text')
-        return el.data;
-      return el.children[0].data;
-    })
-    .join(' ')
-    .replace(/\n/g, ' ')
-    .replace(/\s+/g, ' ')
+  static _description(e) {
+    return e.children
+      .map((el) => {
+        if(el.type === 'text')
+          return el.data;
+        return el.children[0].data;
+      })
+      .join(' ')
+      .replace(/\n/g, ' ')
+      .replace(/\s+/g, ' ');
+  }
+
+}
+
+class Books {
+  constructor($) {
+    this.article = null;
+    this.structure = {};
+    this.count = -1;
+    this.children = {};
+  }
+
+  async extract(url) {
+    const { data: content } = await axios.get(url);
+    const $ = cheerio.load(content);
+    this.article = $('body > div.global-layout > div > article');
+    this.children = this.article.children();
+  }
+
+  transform() {
+    let item = this.iterator().next();
+    while(item) {
+      const { element } = item;
+      if(element) {
+        if(element.type === 'tag') {
+          if(element.name === 'h2') {
+            this.count += 1;
+            this.structure[this.count] = { description: '' };
+            this.structure.length = this.count + 1;
+            this.structure[this.count].title = Book._title(element);
+          }
+          if(element.name === 'div'){
+            this.structure[this.count].language = Book._language(element);
+          };
+          if(element.name === 'a') {
+            this.structure[this.count].isbn = Book._isbn(element);
+          };
+          if(element.name === 'p'){
+            const description = `${this.structure[this.count].description} ${Book._description(element)}`;
+            this.structure[this.count].description = description;
+          }
+        }
+      }
+      item = item.next();
+    }
+  }
+
+  async load() {
+    for (let index = 0; index < this.structure.length; index++) {
+      const element = this.structure[`${index}`];
+      const book = await API.create(element);
+      console.log(`book with id ${book.id} created.`);
+    }
+  }
+
+  next(idx=-1) {
+    const _idx = idx + 1;
+    const element = this.children[`${_idx}`];
+    if (!element)
+      return null;
+    return {
+      element,
+      next: () => this.next(_idx)
+    }; 
+  }
+
+  iterator() {
+    return { next: () => this.next() };
+  }
+}
+
+module.exports = {
+  Books
 }
